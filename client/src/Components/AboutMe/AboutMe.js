3a16aa71b19e9def603aab1f5acae229cc61661c
weicheng lee <z82111712@gmail.com>
2022-03-29 20:57:04 -0700

    update about me component

---
+++ client/src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react'
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
+import ScrollService from '../../utilities/ScrollService'
+import Animation from '../../utilities/Animation'
+import { Link } from 'react-router-dom'
+
+const AboutMe = ({ id }) => {
+    let fadeInScreenHandler = (screen) => {
+        if (screen.fadeScreen !== id) return;
+        Animation.animations.fadeInScreen(id)
+    }
+
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+    const SCREEN_CONSTANTS = {
+        description: "this is description for testing",
+        highlights: {
+            bullets: [
+                "thisqldwklwkl blklwqkdwq",
+                "thisqldwklwkl blklwqkdwq",
+                "thisqldwklwkl blklwqkdwq",
+                "thisqldwklwkl blklwqkdwq",
+                "thisqldwklwkl blklwqkdwq",
+            ],
+            heading: "Here are a few Highlights:"
+        }
+    }
+
+    const renderHighlight = () => {
+        return (
+            SCREEN_CONSTANTS.highlights.bullets.map((value, idx) => (
+                <div className='highlight' key={idx}>
+                    <div className='highlight-blob'></div>
+                    <span>{value}</span>
+                </div>
+            ))
+        )
+    }
+
+    return (
+        <div className='about-me-container screen-container' id={id || ""}>
+            <div className='about-me-parent'>
+                <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'} />
+                <div className='about-me-card'>
+                    {/* <div className='about-me-profile'></div> */}
+                    <div className='about-me-details'>
+                        <span className='about-me-description'>{SCREEN_CONSTANTS.description}</span>
+                        <div className='about-me-highlights'>
+                            <div className='highlight-heading'>
+                                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+                            </div>
+                            {renderHighlight()}
+                        </div>
+                        <div className='about-me-option'>
+                            <button className='btn primary-btn'>
+                                Contact
+                            </button>
+                            {/* access the file thru /Resumexxx.pdf from public folder */}
+                            <Link to="/Resume_Wayne_Lee.pdf" target="_blank">
+                                <button className='btn highlighted-btn'>Resume</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe
